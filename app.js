81c5e3c10fe201b3c15e592bef05986b6c195d76
EddieS42 <eduardo.srocha@hotmail.com>
2017-05-16 01:15:23 -0300

    configure running app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static("node_modules"));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("languageSelector")
+})
+
+app.get("/landing", function(req,res){
+	res.render("index.ejs")
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
